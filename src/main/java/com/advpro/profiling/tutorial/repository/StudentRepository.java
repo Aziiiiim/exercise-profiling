c2c690ce47bc1973e106f8719bdc28e296909199
a23barho <azim.barhoumi@imt-atlantique.net>
2025-03-14 14:13:45 +0700

    Added a method to get the student courses with only 1 query

--- src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
+++ src/main/java/com/advpro/profiling/tutorial/repository/StudentRepository.java
@@ -1,7 +1,11 @@
 package com.advpro.profiling.tutorial.repository;
 
 import com.advpro.profiling.tutorial.model.Student;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 /**
@@ -9,4 +13,8 @@ import org.springframework.stereotype.Repository;
  */
 @Repository
 public interface StudentRepository extends JpaRepository<Student, Long> {
+	
+	@Query("SELECT s FROM Student s JOIN FETCH s.courses sc JOIN FETCH sc.course")
+	List<Student> findAllWithCourses();
+
 }
